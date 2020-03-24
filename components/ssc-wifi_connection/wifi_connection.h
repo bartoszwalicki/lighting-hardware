0ec4820fab557f55eafbe108887ad8b6c513f902
Bartosz Walicki <walicki.bartosz@gmail.com>
2020-03-25 00:19:03 +0100

    Wifi connection moved to the component.

---
+++ components/ssc-wifi_connection/wifi_connection.h
@@ -0,0 +1,34 @@
+#ifndef WIFI_CONNECTION_H
+#define WIFI_CONNECTION_H
+
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+#define EXAMPLE_ESP_WIFI_SSID      CONFIG_ESP_WIFI_SSID
+#define EXAMPLE_ESP_WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
+#define EXAMPLE_ESP_MAXIMUM_RETRY  CONFIG_ESP_MAXIMUM_RETRY
+
+static EventGroupHandle_t s_wifi_event_group;
+
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+
+static const char *TAG = "wifi station";
+
+static int s_retry_num = 0;
+
+void wifi_init_sta(void);
+
+#endif
